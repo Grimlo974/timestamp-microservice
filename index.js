b9a768b86c37090d3f9a46de4f6e46c7b4850b5e
Loïc GRIMAUD <loic.grimaud@outlook.com>
2023-03-04 22:52:41 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    unix: Date.now(),
+    utc: new Date().toUTCString()
+  });
+});
+
+app.get("/api/:date", (req, res) => {
+  let userInput = req.params.date;
+  let dateConverted
+
+  if(/\d{5,}/.test(userInput)){
+    //Timestamps unix
+    let dateFromTimestamp = new Date(parseInt(userInput))
+    dateConverted = { 
+                      unix: Math.floor(dateFromTimestamp.getTime()),
+                      utc: dateFromTimestamp.toUTCString()
+                    }
+  }else{
+    let dateFromString = new Date(userInput)
+    if(dateFromString.toString() === "Invalid Date"){
+      dateConverted = { error : "Invalid Date" }
+    }else{
+      dateConverted = { 
+                        unix: dateFromString.valueOf(),
+                        utc: dateFromString.toUTCString()
+                      }
+    }
+  }
+  
+  res.json(dateConverted)
+});
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
